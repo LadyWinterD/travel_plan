8c1f97308928758b7c0eab50e5c682e73368f7d2
L <31304919+LadyWinterD@users.noreply.github.com>
2025-06-03 11:17:33 +1200

    Add default export to ItineraryPage component

--- src/pages/ItineraryPage.tsx
+++ src/pages/ItineraryPage.tsx
@@ -1,3 +1,14 @@
-{
-  // Full content of ItineraryPage.tsx has been provided in the previous file changes
-}
+import React from 'react';
+
+const ItineraryPage: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Your Itinerary</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">Your travel itinerary will be displayed here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ItineraryPage;
