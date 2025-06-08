604ef3e7bbd1164f415dc457da1fef9c99329d6b
L <31304919+LadyWinterD@users.noreply.github.com>
2025-06-08 17:46:41 +1200

    Updated ActivitiesPage.tsx

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -7,7 +7,7 @@ import { getMockActivities, getWeatherBasedRecommendations, getMockWeather } fro
 
 const WeatherForecastCard: React.FC<{ weather: WeatherData; location: string }> = ({ weather, location }) => {
   const getWeatherIcon = () => {
-    if (weather.isRainy) return <CloudRain className="text-blue-500" size={32} />;
+    if (weather.isRainy) return <CloudRain className="text-blue-500\" size={32} />;
     if (weather.temperature > 25) return <Sun className="text-yellow-500" size={32} />;
     return <Cloud className="text-gray-500" size={32} />;
   };
